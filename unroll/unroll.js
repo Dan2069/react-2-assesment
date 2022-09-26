0b12153e27a6af94cacb5003b93005996ec04837
Daniel <danielmosqueda15@gmail.com>
2022-09-26 03:21:07 -0400

    initial files and first attempt at unroll

---
+++ unroll/unroll.js
@@ -0,0 +1,123 @@
+function unroll(squareArray) {
+
+    // console.log(squareArray);
+    // Needs to start at R1:C1 all the way RIGHT to R1:C4     i (rows) stays the same
+    // Then R1:C4 all the way DOWN to R4: C4        j (column) stays the same
+    // Then R4:C4 all the way LEFT to R4: C1    i(rows) stays the same
+    // Then R4:C1 all the way UP UNTIL R2:C1    j(column) stays the same
+    // Then R2:C1 all the way RIGHT UNTIL R2:C3   i (rows) stays the same
+    // Then R2:C3 all the way DOWN UNTIL R3:C3  j(column) stays the same
+    // Then R3:C3 all the way LEFT UNTIL R3:C2  i (rows) stays the same
+    // FINISHED
+
+    // Each Array within the Square Array can be represented as ROWS
+    // Ex. Row 1 = [1,2,3,4] Row 2 = [5,6,7,8] Row 3 = [9,10,11,12] Row 4 = [13,14,15,16]
+    
+    // Each index of the Rows can be represented as the COLUMNS
+    // Ex. Row 1 = [Column 1, Column 2, Column 3, Column 4]
+
+
+  let myUnRoll = [];
+  let row = 0;
+  let column = 0;
+  let currentPosition = [row,column]
+
+  // RIGHT
+  function Right (i,j){
+  for (let y = j; y < squareArray.length; y++){
+    // console.log("Line 28");
+    let x = i;
+      if(!myUnRoll.includes(squareArray[x][y])){
+      myUnRoll.push(squareArray[x][y])
+
+      currentPosition = [row,column];
+      row = x;
+      column = y;
+      }
+  }
+  } 
+
+
+  // Down
+  function Down (i,j) {
+  for (let x = i; x < squareArray.length; x++){
+    let y = j;
+    if(!myUnRoll.includes(squareArray[x][y])){
+    myUnRoll.push(squareArray[x][y])
+
+    currentPosition = [row,column];
+    row = x;
+    column = y;
+    }
+  }
+  }
+
+
+  // Left
+  function Left (i,j){
+  for (let y = j; y >= 0; y--){ 
+    let x = i
+    if(!myUnRoll.includes(squareArray[x][y])){
+      myUnRoll.push(squareArray[x][y])
+      }
+      currentPosition = [row,column];
+      row = x;
+      column = y;
+    }  
+  }
+
+  // Up
+  function Up (i,j){
+  for (let x = i; x > 0; x--){
+    let y = j;
+    if(!myUnRoll.includes(squareArray[x][y])){
+      myUnRoll.push(squareArray[x][y])
+      }
+      currentPosition = [row,column];
+      row = x;
+      column = y;
+  }
+}
+
+
+  Right(row,column);
+  Moves = Moves + 1
+  Down(row,column);
+  Left(row,column);
+  Up(row,column);
+
+
+
+
+  
+  
+    // console.log(currentPosition);
+    // console.log(row, column);
+    // console.log(myUnRoll);
+    // console.log(myUnRoll.length);
+    // console.log(squareArray.length*squareArray.length);
+  
+
+}
+
+
+const square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+  // 7 Movements
+
+const square2 = [
+  [1,2,3,4,5],
+  [6,7,8,9,10],
+  [11,12,13,14,15],
+  [16,17,18,19,20],
+  [21,22,23,24,25]
+];
+// 9 Movements
+
+// console.log(unroll(square));
+console.log(unroll(square2));
+module.exports = unroll;
